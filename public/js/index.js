38879d6785e24d43f90ae40a51a01695fd0d6277
Edvards <151825634+DarkyBeary@users.noreply.github.com>
2024-09-12 14:30:57 +0300

    Progress: main page

---
+++ public/js/index.js
@@ -0,0 +1,7 @@
+function openLeaderboard() {
+    document.getElementById('leaderboards').classList.remove('hidden');
+}
+
+function closeLeaderboard() {
+    document.getElementById('leaderboards').classList.add('hidden');
+}
