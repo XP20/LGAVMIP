53879ee282ccc48ea8fa0347dd7ae0f07f4668b4
Edvards <151825634+DarkyBeary@users.noreply.github.com>
2024-09-12 13:55:43 +0300

    Progress: main page

--- src/routes/pages.ts
+++ src/routes/pages.ts
@@ -5,6 +5,7 @@ import { serveStatic } from 'hono/bun';
 // Define page routes
 const routesPages = new Hono()
     .get('/', serveStatic({ path: './src/pages/index.html' }))
+    .get('/start', serveStatic({ path: './src/pages/start.html' }))
 
 
 export default routesPages;
