454778856c8719474060a63f13f8e7cb73723715
XP20 <67698795+XP20@users.noreply.github.com>
2024-09-19 15:38:49 +0300

    feat: supabase db leaderboards

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,6 @@
+const PUBLIC_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR4anlkc3R3cXJ3Y2xkb3hlcnZyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjY1ODc3MzIsImV4cCI6MjA0MjE2MzczMn0.sZuQvNh_RLOPSeCVGpG0B6fvdVxI4WKR2gM2fH3bzJ0';
+const _supabase = supabase.createClient('https://txjydstwqrwcldoxervr.supabase.co', PUBLIC_KEY);
+
 function openLeaderboard() {
     document.getElementById('leaderboard').classList.remove('hidden');
 }
@@ -6,26 +9,25 @@ function closeLeaderboard() {
     document.getElementById('leaderboard').classList.add('hidden');
 }
 
-async function getLeaderboard(from = 0, count = 10, reversed = false) {
-    const response = await fetch('/api/leaderboard/test', {
-        method: 'POST',
-        body: JSON.stringify({
-            from: from,
-            count: count,
-            reversed: reversed,
-        }),
-    });
-    const result = await response.json();
+async function getLeaderboard(limit, reversed=false) {
+    const { data, error } = await _supabase.from('leaderboard')
+        .select().order('score', { ascending: reversed }).limit(limit);
+    
+    if (error) {
+        console.error(error);
+        return;
+    }
 
     const leaderboardList = document.getElementById('leaderboard-list');
     leaderboardList.innerHTML = '';
-    
-    for (const entry of result) {
-        const [key, value] = entry;
-        const leaderboardEntry = document.createElement("span");
-        leaderboardEntry.appendChild(document.createTextNode(`${key} - ${value}`));
 
+    for (row of data) {
+        const { user, score } = row;
+        const leaderboardEntry = document.createElement("span");
+        leaderboardEntry.classList.add('text-overflow')
+        leaderboardEntry.appendChild(document.createTextNode(`${user} ${score}`));
+        
         leaderboardList.appendChild(leaderboardEntry);
     }
 }
-getLeaderboard();
+getLeaderboard(10);
