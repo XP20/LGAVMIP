73d9684c8214d5a6de721c8c824fd595e669d885
mark-kruz <markuskruzs@inbox.lv>
2024-09-26 14:55:52 +0300

    Implemented very early gamelike functionality

--- public/js/maps.js
+++ public/js/maps.js
@@ -1,7 +1,9 @@
 let panorama;
 let sv;
 let marker = null;
-let panoLocation
+let panoLocation;
+let score = 0;
+let roundCounter = 0;
 
 function getRndInteger(min, max) {
   return Math.floor(Math.random() * (max - min + 1) ) + min;
@@ -50,9 +52,30 @@ async function doPanorama() {
 }
 window.initMap = initialize;
 
+function computeScore(){
+  const maxScore = 1000; //score value at 0 meters/100% accurate guess
+  const punishmentFactor = 10; //controls how quickly the score drops off with inaccuracy, the closer to 0, the more punishing
+  const distance = google.maps.geometry.spherical.computeDistanceBetween(marker.position, panoLocation);
+  tempScore = maxScore/((distance/(maxScore*punishmentFactor))+1); // asymptotically goes down to 0 as distance from real guess tends to infinity, gives too high of a score for shitty guesses so need a secondary factor to take care of far-guess edge cases
+  tempScore = tempScore - Math.pow(distance, 5)*Math.pow(10, -18); // high-order power that pulls down the score at extreme distances
+  if (tempScore<0) {
+    return 0;
+  } else return Math.ceil(tempScore);
+}
 
 function Submit() {
-  console.log(marker.position.lat(), marker.position.lng(), panoLocation.lat(), panoLocation.lng(), google.maps.geometry.spherical.computeDistanceBetween(marker.position, panoLocation));
+  if (roundCounter<5) {
+    score += computeScore();
+    
+  // console.log(marker.position.lat(), marker.position.lng(), panoLocation.lat(), panoLocation.lng(), score);
+    document.getElementById('score').innerHTML = score.toString();
+    roundCounter+=1;
+  } else {
+    document.getElementById('score').innerHTML = "Final score: " + score.toString();
+    roundCounter=0; //After 5 rounds, reset
+    score=0;
+  }
+  doPanorama();
 }
 
 function toggleDebugMap() {
