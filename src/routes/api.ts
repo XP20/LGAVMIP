a095288977ecbd7495c23087e0c429aef140846a
mark-kruz <markuskruzs@inbox.lv>
2024-11-26 15:50:03 +0200

    Began work on moving game logic to backend

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,10 +1,12 @@
 import { Hono } from "hono";
 import apiLeaderboard from "./api/leaderboard";
+import apiScore from "./api/computeScore";
 
 
 // Define api routes
 const routesApi = new Hono()
-    .route('/leaderboard', apiLeaderboard);
+    .route('/leaderboard', apiLeaderboard)
+    .route('/score', apiScore);
 
 
 export default routesApi;
