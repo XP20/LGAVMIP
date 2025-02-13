ad7569613c0c35e2a8bb38ba3700e43bee1b325c
mark-kruz <markuskruzs@inbox.lv>
2025-02-13 12:54:16 +0200

    Fixed riga gamemode, added valmiera gamemode

--- src/polygons.js
+++ src/polygons.js
@@ -1,22 +1,36 @@
 const { Console } = require('console');
 const fs = require('fs');
+import { parseString } from 'xml2js';
+import { promisify } from 'util';
 
-fs.readFile('./src/locations_raw_data/Riga.geojson', 'utf8', function (err, data) {
-    let place = JSON.parse(data);
-    let maxFeatureIndex = 0
-    while (place.features[maxFeatureIndex+3]!=undefined) {
-        maxFeatureIndex++;
-    }
-    console.log(maxFeatureIndex);
-    coordinatesArr = [];
-    for (let index = 0; index < maxFeatureIndex; index++) {
-        coordinatesArr.push(...place.features[index].geometry.coordinates);
-        console.log(index);
-    //    console.log(place.features[index].geometry.coordinates);
-        
-    }
-    //coordinatesArr = [...place.features[0].geometry.coordinates, ...place.features[1].geometry.coordinates, ...place.features[2].geometry.coordinates, ...place.features[3].geometry.coordinates];
-    console.log(coordinatesArr);
+const parseXMLString = promisify(parseString);
+
+async function parseKMLPolygonCoordinates(kmlString) {
+  try {
+    const result = await parseXMLString(kmlString);
+    
+    // Navigate through the parsed object structure to find coordinates
+    const coordinates = result.kml.Document[0].Placemark[0].Polygon[0]
+      .outerBoundaryIs[0].LinearRing[0].coordinates[0];
+    return coordinates
+      .trim()
+      .split(' ')
+      .filter(coord => coord.trim())
+      .map(coordSet => {
+        const [lng, lat] = coordSet.split(',');
+        return [parseFloat(lng), parseFloat(lat)]
+      });
+  } catch (error) {
+    console.error('Error parsing KML:', error);
+    throw error;
+  }
+}
+
+fs.readFile('./src/locations_raw_data/Valmiera.kml', 'utf8', async function (err, data)  {
+    let string = data.toString();
+    let coordinatesArr = await parseKMLPolygonCoordinates(string);
+   // console.log(coords);
+
     let latitudeArr = [];
     let longitudeArr = [];
     for (let index = 0; index < coordinatesArr.length; index++) {
@@ -28,7 +42,7 @@ fs.readFile('./src/locations_raw_data/Riga.geojson', 'utf8', function (err, data
     let maximumLon = Math.max(...longitudeArr);
     let latLongLiteral = '[{'
     for (let index = 0; index < coordinatesArr.length; index++) {
-        coordinates = coordinatesArr[index];
+        let coordinates = coordinatesArr[index];
         if (index < coordinatesArr.length-1) {
             latLongLiteral += ('lng: ' + coordinates[0] + ', lat: ' + coordinates[1] + '}, {');
         } else {
