9f217e99c72cde0033efd9af2c9c2afaa641279c
mark-kruz <markuskruzs@inbox.lv>
2025-03-19 11:34:54 +0200

    began work on multiplayer 2 electric boogaloo, changed scoring a tiny bit

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -2,6 +2,7 @@ import { Hono } from "hono";
 import apiLeaderboard from "./api/leaderboard";
 import apiScore from "./api/computeScore";
 import apiLocation from "./api/location";
+import apiMP from "./api/MIS2"
 import apiKML from "./api/customGamemode";
 
 
@@ -10,6 +11,7 @@ const routesApi = new Hono()
     .route('/leaderboard', apiLeaderboard)
     .route('/score', apiScore)
     .route('/location', apiLocation)
+    .route('/mp', apiMP)
     //.route('/uploadKML', apiKML);
 
 
--- src/routes/api/computeScore.ts
+++ src/routes/api/computeScore.ts
@@ -1,20 +1,20 @@
 import { Hono } from "hono";
 
 //this should also be in database probably
-let punishmentFactors = [5,8,8,12,6,5];
+const punishmentFactors = [5,8,8,12,6,5];
 
 const apiScore = new Hono()
     .post('/', async (c) => {
         const body = await c.req.json();
-        const {distance, gamemode} = body;
+        let {distance, gamemode} = body;
         const maxScore = 1000; //score value at 0 meters/100% accurate guess
         const punishmentFactor = punishmentFactors[gamemode]; //controls how quickly the score drops off with inaccuracy, the closer to 0, the more punishing
+        if (distance>50) distance -= 50;
         let tempScore = maxScore/((distance/(maxScore*punishmentFactor))+1); // asymptotically goes down to 0 as distance from real guess tends to infinity, gives too high of a score for shitty guesses so need a secondary factor to take care of far-guess edge cases
         tempScore = tempScore - Math.pow(distance, 5)*Math.pow(10, -2*punishmentFactor); // high-order power that pulls down the score at extreme distances
         if (tempScore<0) tempScore=0;
         let score = Math.ceil(tempScore);
         if (distance<50) score = 1000;
-
         const a = {score:score};
         return c.json(a);
     });
