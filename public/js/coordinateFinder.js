39459ab06ca1a177aec8d830225dac686270139d
mark-kruz <markuskruzs@inbox.lv>
2025-04-14 16:13:53 +0300

    Continued work on MOTD

--- public/js/coordinateFinder.js
+++ public/js/coordinateFinder.js
@@ -2,7 +2,7 @@ function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
 
-function setGamemode(num) {
+function setGamemode(num) { //Is there a good reson for this to be in coordinateFinder?????????
     gamemode=num;
     initialize();
 }
