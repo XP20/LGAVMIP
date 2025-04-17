01e984728285d742eb0cb2121c1c85ec85a298df
mark-kruz <markuskruzs@inbox.lv>
2025-04-17 16:14:16 +0300

    Disabled seed input for now (entering a negative number in this field causes the server to enter a death-loop, highly suboptimal)

--- src/routes/api/motd.js
+++ src/routes/api/motd.js
@@ -1,7 +1,7 @@
 import { Hono } from "hono";
 const apiMOTD = new Hono();
 
-const motds = ['Stabilāks par Latvija.lv!', 'Iedvesmots no VPS!', 'Labāks par EDS!', 'Ciešamāks par E-Veselību!', 'Vidusskolēnu veidots!', 'Neizmanto React!', 'Neizmanto Vue!', 'Neizmanto jQuery!', 'Neizmanto sīkdatnes!', 'Nav SaaS!', 'Hostots no veca laptopa!', 'Nenobrūk katru martu!', 'Neprasa eID karti!', 'Neprasa eParakstu!', 'Ieviests ātrāk par Rail Baltica!', 'Nav jāgaida līdz nākamajām vēlēšanām!', 'Strādā arī svētku dienās!', 'Neliek gaidīt e-rindā!', 'Nevajag iesniegt iesniegumu!', 'Nav jākārto spēlēšanas atļauja!', 'Netērē nodokļu maksātāju naudu!', 'Nav katru gadu jāveic jauns iepirkums!']
+const motds = ['Stabilāks par Latvija.lv!', 'Iedvesmots no VPS!', 'Labāks par EDS!', 'Ciešamāks par E-Veselību!', 'Vidusskolēnu veidots!', 'Neizmanto React!', 'Neizmanto Vue!', 'Neizmanto jQuery!', 'Neizmanto sīkdatnes!', 'Nav SaaS!', 'Hostots no veca laptopa!', 'Nenobrūk katru martu!', 'Neprasa eID karti!', 'Neprasa eParakstu!', 'Ieviests ātrāk par Rail Baltica!', 'Nav jāgaida līdz nākamajām vēlēšanām!', 'Strādā arī svētku dienās!', 'Neliek gaidīt e-rindā!', 'Nevajag iesniegt iesniegumu!', 'Nav jākārto spēlēšanas atļauja!', 'Netērē nodokļu maksātāju naudu!', 'Nav katru gadu jāveic jauns iepirkums!', 'Nav iepirkts no Čehijas!', 'Nav jāparbūvē kontakttīkls!', 'Nav jāpārbūvē sliedes!']
 
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
